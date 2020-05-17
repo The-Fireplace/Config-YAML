b8fef46ba1d8ab84603f3420103ac2a06d519d54
The_Fireplace <the.f1repl4ce@gmail.com>
2020-05-17 15:35:27 -0500

    Add a little documentation

--- src/main/java/the_fireplace/configyaml/YAMLComment.java
+++ src/main/java/the_fireplace/configyaml/YAMLComment.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
 
+/**
+ * An interface for adding comments to your YAML. Annotate fields you want commented with this. Use \n to separate lines for multiline comments.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(FIELD)
 public @interface YAMLComment {
